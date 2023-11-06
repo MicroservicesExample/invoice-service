441b9e4ba8cab3e0baa93624852bf60b295875d1
Ashok <ashok.m.mane@gmail.com>
2023-11-06 18:38:17 +0530

    ports updated

--- src/main/java/org/ashok/invoiceservice/InvoiceServiceApplication.java
+++ src/main/java/org/ashok/invoiceservice/InvoiceServiceApplication.java
@@ -1,13 +1,9 @@
 package org.ashok.invoiceservice;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
